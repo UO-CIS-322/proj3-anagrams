9fbbaa9fb48025d19ae32a13245a9cc69a5195d8
Michal Young <michal.young@gmail.com>
2017-10-05 07:17:39 -0700

    Moving files for new organization

---
+++ tests/test_jumble.py
@@ -0,0 +1,28 @@
+"""
+Nose tests for jumble.py
+
+We cannot test for randomness here (no effective oracle),
+but we can test that the elements in the returned string
+are correct.
+"""
+
+from jumble import jumbled
+
+def same(s, t):
+    """
+    Same characters (possibly in different order)
+    in two strings s and t.
+    """
+    return sorted(s) == sorted(t)
+
+def test_jumbled_single():
+    assert same( jumbled(["abbcd"], 1), "abbcd" )
+
+def test_jumbled_pair():
+    assert same( jumbled(["abbc", "abcc"], 2), "abbcc")
+
+def test_jumbled_more():
+    assert same( jumbled(["aabc", "abac", "bcaa"], 2), "aabc")
+
+    
+
